56b347c35c787935b09674cd9fb035ccc4def9fd
ocarinayoy <ocarinayoy12@gmail.com>
2023-06-25 13:35:58 -0600

    Initial commit

---
+++ src/main/java/net/ocarinayoy/ocarinascrate/Ocarina_sCrate.java
@@ -0,0 +1,13 @@
+package net.ocarinayoy.ocarinascrate;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Ocarina_sCrate implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
